26afaf7a5a71efc18809532e30655fe5bff1677a
Hosam El Nagar <34396668+hosamn@users.noreply.github.com>
2022-01-11 14:02:57 +0200

    Add files via upload

---
+++ layers/Map_OutlineClip_mask_13.js
@@ -0,0 +1,8 @@
+var json_Map_OutlineClip_mask_13 = {
+"type": "FeatureCollection",
+"name": "Map_OutlineClip_mask_13",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ 40.595783418464343, 32.452342839681272 ], [ 40.810700738255022, -7.092444001804303 ], [ 22.471089449450119, -6.949165788610514 ], [ 22.399450342853228, 32.667260159471958 ], [ 40.595783418464343, 32.452342839681272 ] ] ] ] } }
+]
+}
---
+++ styles/Rivers_ClippedBy_Basin_6_style.js
@@ -0,0 +1,31 @@
+var size = 0;
+var placement = 'point';
+
+var style_Rivers_ClippedBy_Basin_6 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = ""
+    var labelText = "";
+    size = 0;
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'line';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    var style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(72,123,182,0.7333333333333333)', lineDash: [10,5], lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
