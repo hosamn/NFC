ef68f59dc43a7c643d6e41f7adc2acffc5ce1b2f
Hosam <34396668+hosamn@users.noreply.github.com>
2022-07-03 14:43:55 +0200

    Add files via upload
    File Submitter

---
+++ gitAddCommitPush.bat
@@ -0,0 +1,30 @@
+cd NFC
+
+git add data
+
+git commit -m "data update" -- data
+
+git push origin main
+
+echo DONE!
+pause
+
+
+
+REM git clone https://github.com/hosamn/NFC.git
+REM # clones the remote repo locally
+
+REM git remote add origin  https://github.com/hosamn/NFC.git
+REM # Sets the new remote
+
+REM git remote -v
+REM # Verifies the new remote URL
+
+REM git add data
+REM # adds the 'data' folder to staging area
+
+REM git commit -m 'data update' -- data
+REM # commits the 'data' folder changes
+
+REM git push origin main
+REM # Pushes the changes in your local repository up to the remote repository you specified as the origin
